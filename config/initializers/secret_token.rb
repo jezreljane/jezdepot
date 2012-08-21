5235be9335df8dad465d1bb3b713c72010322233
Jezrel Jane <07jezrel@gmail.com>
2012-08-22 07:18:13 +0800

    Initial Commit of Jez Depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jezdepot::Application.config.secret_token = '8c14ec206325b7f9a1715b4065ddad365c2264c43a359634c123a1960972693a55b58ff9d17b650ad8db198f598f0d94ae32a1d2aaca5566afda2b63f32683e1'
