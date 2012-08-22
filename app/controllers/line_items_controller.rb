0772dabb3705e140d58daf34c14133b4fab82930
Jezrel Jane <07jezrel@gmail.com>
2012-08-22 08:58:55 +0800

    Smarter Cart--with quantities

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,8 +42,9 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product_id: product)
-    @line_item.product = product
+    #@line_item = @cart.line_items.build(product_id: product)
+    @line_item = @cart.add_product(product.id)
+    #@line_item = @cart.add_product(product_id: product.id)
 
     respond_to do |format|
       if @line_item.save
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ActiveRecord::Base
   #attr_accessible :cart_id, :product_id
 
-  attr_accessible :product_id, :id, :cart_id, :created_at, :updated_at
+  attr_accessible :id, :cart_id, :product_id, :created_at, :updated_at, :product
 
   belongs_to :product
   belongs_to :cart
